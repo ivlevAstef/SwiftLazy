cfddf781c4131906533e29570cf79c2c7cd96fdd
Ivlev Alexander <ivlev.stef@gmail.com>
2019-01-29 12:50:05 +0700

    update tabulation

--- Package.swift
+++ Package.swift
@@ -3,12 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftLazy",
-  products: [.library(name: "SwiftLazy",
-					  targets: ["SwiftLazy"])
-	],
-  targets: [
-	.target(name: "SwiftLazy",
-			dependencies: []),
-	]
+  products: [.library(name: "SwiftLazy", targets: ["SwiftLazy"])],
+  targets: [.target(name: "SwiftLazy", dependencies: [])]
 )
 
