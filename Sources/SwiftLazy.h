00e069eb819eea9fc07ed933c3ce3f68cb4c7cdc
Ивлев А.Е <ae.ivlev@tensor.ru>
2019-09-04 15:48:45 +0700

    remake project file

--- Sources/SwiftLazy.h
+++ Sources/SwiftLazy.h
@@ -14,4 +14,6 @@ FOUNDATION_EXPORT double SwiftLazyVersionNumber;
 //! Project version string for SwiftLazy.
 FOUNDATION_EXPORT const unsigned char SwiftLazyVersionString[];
 
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLazy/PublicHeader.h>
+
 
