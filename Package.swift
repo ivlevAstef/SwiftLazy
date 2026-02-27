9778955f876869dc9d494d6f707103f6767cecae
Nikita Patskov <patskovn@yahoo.com>
2026-02-27 08:16:11 +0000

    Add Linux support
    - Remove `platforms` restriction from Package.swift (was Apple-only)
    - Add platform-conditional imports for pthread types in BaseLazy.swift
      (Darwin/Glibc/Musl) so `pthread_rwlock_t` resolves on Linux

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftLazy",
-  platforms: [.iOS(.v13), .watchOS(.v8), .macOS(.v10_15), .tvOS(.v13)],
   products: [.library(name: "SwiftLazy", targets: ["SwiftLazy"])],
   targets: [.target(name: "SwiftLazy", dependencies: [])]
 )
